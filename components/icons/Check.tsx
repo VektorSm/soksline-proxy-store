4199926a5d6dd5925adc7ba666462d3a3269541f
VektorSm <astafev.viktor@gmail.com>
2025-10-06 00:09:01 +0400

    feat: refresh order builder layout

---
+++ components/icons/Check.tsx
@@ -0,0 +1,5 @@
+export const Check = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" {...props}>
+    <path d="M7.8 13.4 4.7 10.3 3.3 11.7l4.5 4.5 9-9-1.4-1.4-7.6 7.6z" />
+  </svg>
+);
